c0533da842e5ca878e8ab12b6c271285a97abc63
EndyKaufman <admin@site15.ru>
2019-01-26 14:50:44 +0500

    chore(version): Release

--- test/index.test.ts
+++ test/index.test.ts
@@ -33,14 +33,14 @@ describe('ngx-bind-io-cli', () => {
   test
     .stdout()
     .do(() => cmd.run(['test/fixtures/sources', '--maxInputs=0', '--maxOutputs=0']))
-    .it('test/fixtures/sources', ctx => {
+    .it(['test/fixtures/sources', '--maxInputs=0', '--maxOutputs=0'].join(' '), ctx => {
       expect(ctx.stdout).to.contain(`<inherits-inner-cmp>: Not initialized inputs: isLoading`);
       expect(ctx.stdout).to.contain(`Not ready for migrate to NgxBindIO: 1 components`);
     });
   test
     .stdout()
     .do(() => cmd.run(['test/fixtures/sources', '--info', '--maxInputs=0', '--maxOutputs=0']))
-    .it('test/fixtures/sources', ctx => {
+    .it(['test/fixtures/sources', '--info', '--maxInputs=0', '--maxOutputs=0'].join(' '), ctx => {
       expect(ctx.stdout).to.contain(`<inherits-inner-cmp>: Inputs: isLoading, propA, propB`);
       expect(ctx.stdout).to.contain(`<inherits-inner-cmp>: Outputs: start`);
       expect(ctx.stdout).to.contain(`<inherits-inner-cmp>: Not initialized inputs: isLoading`);
@@ -49,9 +49,9 @@ describe('ngx-bind-io-cli', () => {
   test
     .stdout()
     .do(() => cmd.run(['test/fixtures/sources', '--info', '--verbose', '--maxInputs=0', '--maxOutputs=0']))
-    .it('test/fixtures/sources', ctx => {
+    .it(['test/fixtures/sources', '--info', '--verbose', '--maxInputs=0', '--maxOutputs=0'].join(' '), ctx => {
       expect(ctx.stdout).to.contain(`<basic-inner-cmp>: Class: InnerComponent`);
-      expect(ctx.stdout).to.contain(`<basic-inner-cmp>: Used with "bindIO" directive in 1 places`);
+      expect(ctx.stdout).to.contain(`<basic-inner-cmp>: Used with "bindIO" directive in 2 places`);
       expect(ctx.stdout).to.contain(`<inherits-inner-cmp>: Class: InnerComponent`);
       expect(ctx.stdout).to.contain(`<inherits-inner-cmp>: Used with "bindIO" directive in 1 places`);
       expect(ctx.stdout).to.contain(`<inherits-inner-cmp>: Inputs: isLoading, propA, propB`);
@@ -62,7 +62,7 @@ describe('ngx-bind-io-cli', () => {
   test
     .stdout()
     .do(() => cmd.run(['test/fixtures/sources', '--info', '--fix', '--maxInputs=0', '--maxOutputs=0']))
-    .it('test/fixtures/sources', ctx => {
+    .it(['test/fixtures/sources', '--info', '--fix', '--maxInputs=0', '--maxOutputs=0'].join(' '), ctx => {
       expect(ctx.stdout).to.contain(`<inherits-inner-cmp>: Inputs: isLoading, propA, propB`);
       expect(ctx.stdout).to.contain(`<inherits-inner-cmp>: Outputs: start`);
       expect(ctx.stdout).to.contain(`<inherits-inner-cmp>: Auto initialized inputs: isLoading`);
